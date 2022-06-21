cb7a4a939fa8d7f043f295c56c8772091bf2d6cf
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-06-21 11:46:26 +0200

    Project without .gitignore

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,52 +1,46 @@
 ﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
 using System.Device.Gpio;
-using System.Threading;
-using System.Net;
-using System.Collections.Specialized;
-using System.Text;
 
+namespace CCheck{
+    class Program{
 
+        static void Main(){
+            const string uri = "https://rpitoilet.azurewebsites.net/api/AddTime?code=dCcBlx9n8AIn7VufImhzUG152vjqETeDWeSXhW1ZDbLnAzFulQNSVw==";
+            var client = new RestClient(uri);
+            var request = new RestRequest();
+            DateTime now = DateTime.UtcNow;
+            const int GREEN_PIN = 3;
+            const int RED_PIN = 27;
+            const int PIR_PIN = 4;
+            bool occupied = false;
+            var controller = new GpioController();
+            controller.OpenPin(GREEN_PIN, PinMode.Output);
+            controller.OpenPin(RED_PIN, PinMode.Output);
+            controller.OpenPin(PIR_PIN, PinMode.Input);
 
-const int GREEN_PIN = 3;
-const int RED_PIN = 27;
-const int PIR_PIN = 4;
-bool occupied = false;
-var url ="https://maliniak.azurewebsites.net/api/HttpTrigger_rpi?code=qgILQFmfaUNyHqM3_J0QXK14oX5LO9c-qo2oW7C4OTwxAzFu9w8gAg==";
-var data = new NameValueCollection();
-var controller = new GpioController();
-controller.OpenPin(GREEN_PIN, PinMode.Output);
-controller.OpenPin(RED_PIN, PinMode.Output);
-controller.OpenPin(PIR_PIN, PinMode.Input);
+            controller.RegisterCallbackForPinValueChangedEvent(PIR_PIN, PinEventTypes.Rising, (sender, args) =>
+            {
+                occupied = !occupied;
+                if(occupied == true){
+                    controller.Write(RED_PIN, PinValue.High);
+                    controller.Write(GREEN_PIN, PinValue.Low);
+                    Console.WriteLine("zajete");
 
-controller.RegisterCallbackForPinValueChangedEvent(PIR_PIN, PinEventTypes.Rising, (sender, args) =>
-{
-    occupied = !occupied;
-    if(occupied == true){
-        data["status"] = "zajete";
+                }
+                else{
+                    controller.Write(RED_PIN, PinValue.Low);
+                    controller.Write(GREEN_PIN, PinValue.High);
+                    Console.WriteLine("wolne"); 
+                }
+                string TimeNow = now.ToString("yyyy-MM-ddTHH\\:mm\\:ss");
+                var body = new {time = TimeNow};
+                request.AddParameter("text/json", body, ParameterType.RequestBody);
+                var response = client.Post(request);
+                Console.WriteLine(response.Content);
+            });
 
-        controller.Write(RED_PIN, PinValue.High);
-        controller.Write(GREEN_PIN, PinValue.Low);
-        Console.WriteLine("zajete");
-        
-    }
-    else{
-        data["status"] = "wolne";
-        controller.Write(RED_PIN, PinValue.Low);
-        controller.Write(GREEN_PIN, PinValue.High);
-        Console.WriteLine("wolne");
-            
-    }
-
-    using (var wb = new WebClient()){
-            var response = wb.UploadValues(url, "POST", data);
-            string responseInString = Encoding.UTF8.GetString(response);
-            Console.WriteLine(responseInString);
         }
-});
-
-
-Console.WriteLine("Awaiting for user action...");
-Console.ReadLine();
-controller.Write(GREEN_PIN, PinValue.Low);
-controller.Write(RED_PIN, PinValue.Low);
-controller.Dispose();
+    }
+}
