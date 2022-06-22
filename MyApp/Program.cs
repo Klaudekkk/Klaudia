4b8be453526015f147355cb5de5d17037e3785ee
DESKTOP-50B3PKI\anubu <anubus.wolf@gmail.com>
2022-06-22 11:53:20 +0200

    fixed rpi program to send proper date

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,13 +4,9 @@ using RestSharp;
 using System.Device.Gpio;
 
 namespace CCheck{
+    
     class Program{
-
         static void Main(){
-            const string uri = "https://rpitoilet.azurewebsites.net/api/AddTime?code=dCcBlx9n8AIn7VufImhzUG152vjqETeDWeSXhW1ZDbLnAzFulQNSVw==";
-            var client = new RestClient(uri);
-            var request = new RestRequest();
-            DateTime now = DateTime.UtcNow;
             const int GREEN_PIN = 3;
             const int RED_PIN = 27;
             const int PIR_PIN = 4;
@@ -25,23 +21,35 @@ namespace CCheck{
             {
                 occupied = !occupied;
                 if(occupied == true){
+                    Post();
                     controller.Write(RED_PIN, PinValue.High);
                     controller.Write(GREEN_PIN, PinValue.Low);
                     Console.WriteLine("zajete");
 
                 }
                 else{
+                    Post();
                     controller.Write(RED_PIN, PinValue.Low);
                     controller.Write(GREEN_PIN, PinValue.High);
-                    Console.WriteLine("wolne"); 
+                    Console.WriteLine("wolne");
                 }
-                string TimeNow = now.ToString("yyyy-MM-ddTHH\\:mm\\:ss");
-                var body = new {time = TimeNow};
-                request.AddParameter("text/json", body, ParameterType.RequestBody);
-                var response = client.Post(request);
-                Console.WriteLine(response.Content);
+                
             });
+        Console.ReadLine();
+        }
 
+        private static void Post(){
+            const string uri = "https://rpitoilet.azurewebsites.net/api/AddTime?code=dCcBlx9n8AIn7VufImhzUG152vjqETeDWeSXhW1ZDbLnAzFulQNSVw==";
+            var client = new RestClient(uri);
+            var request = new RestRequest();
+            DateTime now = DateTime.UtcNow;
+            now = DateTime.UtcNow;
+            now = now.AddHours(2);
+            string TimeNow = now.ToString("yyyy-MM-ddTHH\\:mm\\:ss");
+            var body = new {time = TimeNow};
+            request.AddParameter("text/json", body, ParameterType.RequestBody);
+            var response = client.Post(request);
+            Console.WriteLine(response.Content);
         }
     }
 }
